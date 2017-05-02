22a6cd073757b7291deda7b74bc059aef5cfc992
Vivek Venkatraman Krishnan <vivekvenkris@gmail.com>
2017-05-03 00:49:55 +1000

    Implementations to the last commit.

--- src/Coincidencer.cpp
+++ src/Coincidencer.cpp
@@ -14,11 +14,9 @@ using namespace std;
 
 void* Coincidencer::candidates_client(void* ptr){
 
-	cerr << "Inside client thread..." << endl;
-
 	Coincidencer*  coincidencer = reinterpret_cast< Coincidencer* >(ptr);
 
-	cerr << "From client thread. " << coincidencer->this_candidates.cands.size() << endl;
+	cerr << "Coincidencer Client:: Sending " << coincidencer->this_candidates.cands.size() << "candidates to all other nodes." << endl;
 
 
 	for( string node: ConfigManager::other_active_nodes() ){
@@ -27,18 +25,17 @@ void* Coincidencer::candidates_client(void* ptr){
 
 		ostringstream oss;
 
-		//ClientSocket client_socket ( node, ConfigManager::coincidencer_ports().at(node) );
-		ClientSocket client_socket ( "127.0.0.1", ConfigManager::coincidencer_ports().at(ConfigManager::this_host()) );
+		ClientSocket client_socket ( node , ConfigManager::coincidencer_ports().at(node) );
+		//ClientSocket client_socket ( "127.0.0.1", ConfigManager::coincidencer_ports().at(ConfigManager::this_host()) );
 
-		//oss << ConfigManager::this_host() << " ";
-		oss << node << " ";
+		oss << ConfigManager::this_host() << " ";
+		//oss << node << " ";
 
 		oss << coincidencer->this_candidates << " ";
 
-		cerr << "sending  " << oss.str().size()  << " characters" << endl;
+		//cerr << "sending  " << oss.str().size()  << " characters" << endl;
 
 		client_socket << oss.str();
-
 
 	}
 
@@ -56,17 +53,17 @@ void* Coincidencer::candidates_client(void* ptr){
  */
 void* Coincidencer::candidates_server(void* ptr){
 
-	std::cerr << "Inside server thread ... " << endl;
+	cerr << "Inside server thread ... " << endl;
 
 	Coincidencer*  coincidencer = reinterpret_cast< Coincidencer* >(ptr);
 
-	std::cerr << "From server thread. " << coincidencer->candidate_collection_map->size() << endl;
+	cerr << "Starting server on localhost:" << ConfigManager::coincidencer_ports().at(ConfigManager::this_host()) << endl;
 
-	ServerSocket* socket = new ServerSocket( "127.0.0.1" , ConfigManager::coincidencer_ports().at(ConfigManager::this_host()));
+	ServerSocket* socket = new ServerSocket( "any" , ConfigManager::coincidencer_ports().at(ConfigManager::this_host()));
 
 	while(true){
 
-		int waiting_connections = socket->select(1.0);
+		int waiting_connections = socket->select(5.0);
 
 		if(waiting_connections > 0 ){
 
@@ -76,15 +73,17 @@ void* Coincidencer::candidates_server(void* ptr){
 
 			try{
 
-				string socket_data;
 				ostringstream oss;
 
+				string socket_data;
 				conversational_socket >> socket_data;
 				oss << socket_data;
 
-				cerr << "Got " << socket_data.size() << " characters" <<  endl;
+				//cerr << "Got " << socket_data.size() << " characters" <<  endl;
 
 				std::istringstream iss(oss.str());
+
+				//cerr << "iss:" << iss.str() << endl;
 
 				string node;
 				iss >> node;
@@ -92,9 +91,13 @@ void* Coincidencer::candidates_server(void* ptr){
 				CandidateCollection cc;
 				iss >> cc;
 
-				coincidencer->candidate_collection_map->insert(map<string,CandidateCollection>::value_type(node,cc));
 
-				if(coincidencer->candidate_collection_map->size() == ConfigManager::other_active_nodes().size()){
+
+				coincidencer->other_candidates.cands.insert(coincidencer->other_candidates.cands.end(), cc.cands.begin(), cc.cands.end());
+
+				coincidencer->candidate_collection_map.insert(map<string,CandidateCollection>::value_type(node,cc));
+
+				if(coincidencer->candidate_collection_map.size() == ConfigManager::other_active_nodes().size()){
 					cerr << "Server got data from all nodes. Quitting server" <<  endl;
 					break;
 				}
@@ -111,14 +114,20 @@ void* Coincidencer::candidates_server(void* ptr){
 	return NULL;
 }
 
+int Coincidencer::send_candidates_to_all_nodes(){
+	int start_client = pthread_create(&client, NULL, Coincidencer::candidates_client, (void*) this);
+	return ErrorChecker::check_pthread_create_error(start_client, "Coincidencer send_candidates_to_all_nodes -- starting client");
+
+}
+
 int Coincidencer::gather_all_candidates(){
 
-	int start_client = pthread_create(&client, NULL, Coincidencer::candidates_client, (void*) this);
-
+	cerr << "Waiting for the server to get all candidates." << endl;
 	pthread_join(server,NULL);
+
+	cerr << "Waiting for client to send all candidates." << endl;
 	pthread_join(client,NULL);
 
-	cerr << "Exiting coincidencer. Everything is awesome! " << endl;
 
 	return EXIT_SUCCESS;
 
@@ -131,15 +140,50 @@ void Coincidencer::init_this_candidates(CandidateCollection c){
 
 
 int Coincidencer::coincidence(){
+
+	double freq_bin_width = 1;
+
+	for(Candidate mine: this_candidates.cands){
+
+		int rfi_score = 0;
+		int num_similar_candidates = 0;
+
+		for(Candidate other: other_candidates.cands){
+
+			if( abs(mine.freq - other.freq) < bin_width) { // same frequency
+
+				num_similar_candidates++;
+
+				if( abs(mine.start_fanbeam - other.start_fanbeam) > max_fanbeam_traversal) rfi_score++;
+				if( abs( mine.start_ns - other.start_ns ) > 0.5 ) rfi_score++;
+				if(mine.dm == 0) rfi_score++;
+
+			}
+
+		}
+
+		if(rfi_score <= num_similar_candidates ){
+
+			shortlisted_candidates.append(mine);
+		}
+
+
+	}
+
 	return EXIT_SUCCESS;
 }
+
+void Coincidencer::print_shortlisted_candidates(FILE* fo){
+
+	shortlisted_candidates.print_cand_file(fo,0);
+
+}
+
 
 
 
 Coincidencer::~Coincidencer() {
 
-	delete shortlisted_candidates;
-	delete candidate_collection_map;
 
 }
 
